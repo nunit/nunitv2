629ee6687553a6abc559ecd3beb35d5f462a2c39
charliepoole <>
2003-08-20 05:31:17 +0000

    Update version to 2.1.3

--- samples/vb/AssemblyInfo.vb
+++ samples/vb/AssemblyInfo.vb
@@ -28,5 +28,5 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("2.1.2.0")> 
+<Assembly: AssemblyVersion("2.1.3.0")> 
 
