f34de53027c22ea066a2ba6682a2a4a74f62285b
charliepoole <>
2004-02-23 07:28:04 +0000

    Add test description

--- src/GuiComponents/UiKit/TestPropertiesDialog.cs
+++ src/GuiComponents/UiKit/TestPropertiesDialog.cs
@@ -61,7 +61,6 @@ namespace NUnit.UiKit
 		private System.Windows.Forms.Label label2;
 		private CP.Windows.Forms.ExpandingLabel fullName;
 		private System.Windows.Forms.Label label1;
-		private System.Windows.Forms.Label leakage;
 		private CP.Windows.Forms.ExpandingLabel stackTrace;
 		private System.Windows.Forms.Label label12;
 		private System.Windows.Forms.Label elapsedTime;
@@ -71,6 +70,8 @@ namespace NUnit.UiKit
 		private System.Windows.Forms.Label testResult;
 		private System.Windows.Forms.Label label3;
 		private System.Windows.Forms.CheckBox pinButton;
+		private System.Windows.Forms.Label label6;
+		private CP.Windows.Forms.ExpandingLabel description;
 		private System.ComponentModel.IContainer components;
 
 		#endregion
@@ -115,7 +116,6 @@ namespace NUnit.UiKit
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
 			this.tabControl1 = new System.Windows.Forms.TabControl();
 			this.testTab = new System.Windows.Forms.TabPage();
 			this.testCaseCount = new System.Windows.Forms.Label();
@@ -129,12 +129,13 @@ namespace NUnit.UiKit
 			this.resultsTab = new System.Windows.Forms.TabPage();
 			this.label3 = new System.Windows.Forms.Label();
 			this.testResult = new System.Windows.Forms.Label();
-			this.leakage = new System.Windows.Forms.Label();
 			this.stackTrace = new CP.Windows.Forms.ExpandingLabel();
 			this.label12 = new System.Windows.Forms.Label();
 			this.elapsedTime = new System.Windows.Forms.Label();
 			this.message = new CP.Windows.Forms.ExpandingLabel();
 			this.pinButton = new System.Windows.Forms.CheckBox();
+			this.label6 = new System.Windows.Forms.Label();
+			this.description = new CP.Windows.Forms.ExpandingLabel();
 			this.tabControl1.SuspendLayout();
 			this.testTab.SuspendLayout();
 			this.resultsTab.SuspendLayout();
@@ -147,14 +148,16 @@ namespace NUnit.UiKit
 				| System.Windows.Forms.AnchorStyles.Right)));
 			this.tabControl1.Controls.Add(this.testTab);
 			this.tabControl1.Controls.Add(this.resultsTab);
-			this.tabControl1.Location = new System.Drawing.Point(8, 8);
+			this.tabControl1.Location = new System.Drawing.Point(7, 7);
 			this.tabControl1.Name = "tabControl1";
 			this.tabControl1.SelectedIndex = 0;
-			this.tabControl1.Size = new System.Drawing.Size(416, 144);
+			this.tabControl1.Size = new System.Drawing.Size(346, 142);
 			this.tabControl1.TabIndex = 13;
 			// 
 			// testTab
 			// 
+			this.testTab.Controls.Add(this.description);
+			this.testTab.Controls.Add(this.label6);
 			this.testTab.Controls.Add(this.testCaseCount);
 			this.testTab.Controls.Add(this.label5);
 			this.testTab.Controls.Add(this.label4);
@@ -163,33 +166,33 @@ namespace NUnit.UiKit
 			this.testTab.Controls.Add(this.label2);
 			this.testTab.Controls.Add(this.fullName);
 			this.testTab.Controls.Add(this.label1);
-			this.testTab.Location = new System.Drawing.Point(4, 25);
+			this.testTab.Location = new System.Drawing.Point(4, 22);
 			this.testTab.Name = "testTab";
-			this.testTab.Size = new System.Drawing.Size(408, 115);
+			this.testTab.Size = new System.Drawing.Size(338, 116);
 			this.testTab.TabIndex = 0;
 			this.testTab.Text = "Test";
 			// 
 			// testCaseCount
 			// 
-			this.testCaseCount.Location = new System.Drawing.Point(104, 48);
+			this.testCaseCount.Location = new System.Drawing.Point(87, 70);
 			this.testCaseCount.Name = "testCaseCount";
-			this.testCaseCount.Size = new System.Drawing.Size(48, 16);
+			this.testCaseCount.Size = new System.Drawing.Size(40, 13);
 			this.testCaseCount.TabIndex = 27;
 			// 
 			// label5
 			// 
-			this.label5.Location = new System.Drawing.Point(16, 80);
+			this.label5.Location = new System.Drawing.Point(13, 96);
 			this.label5.Name = "label5";
-			this.label5.Size = new System.Drawing.Size(72, 16);
+			this.label5.Size = new System.Drawing.Size(67, 14);
 			this.label5.TabIndex = 26;
 			this.label5.Text = "Reason:";
 			this.label5.TextAlign = System.Drawing.ContentAlignment.TopRight;
 			// 
 			// label4
 			// 
-			this.label4.Location = new System.Drawing.Point(16, 48);
+			this.label4.Location = new System.Drawing.Point(13, 70);
 			this.label4.Name = "label4";
-			this.label4.Size = new System.Drawing.Size(72, 16);
+			this.label4.Size = new System.Drawing.Size(67, 14);
 			this.label4.TabIndex = 25;
 			this.label4.Text = "Test Count:";
 			this.label4.TextAlign = System.Drawing.ContentAlignment.TopRight;
@@ -200,24 +203,24 @@ namespace NUnit.UiKit
 				| System.Windows.Forms.AnchorStyles.Left) 
 				| System.Windows.Forms.AnchorStyles.Right)));
 			this.ignoreReason.CopySupported = true;
-			this.ignoreReason.Location = new System.Drawing.Point(104, 80);
+			this.ignoreReason.Location = new System.Drawing.Point(87, 95);
 			this.ignoreReason.Name = "ignoreReason";
-			this.ignoreReason.Size = new System.Drawing.Size(296, 16);
+			this.ignoreReason.Size = new System.Drawing.Size(244, 10);
 			this.ignoreReason.TabIndex = 24;
 			// 
 			// shouldRun
 			// 
-			this.shouldRun.Location = new System.Drawing.Point(328, 48);
+			this.shouldRun.Location = new System.Drawing.Point(273, 70);
 			this.shouldRun.Name = "shouldRun";
-			this.shouldRun.Size = new System.Drawing.Size(32, 16);
+			this.shouldRun.Size = new System.Drawing.Size(27, 13);
 			this.shouldRun.TabIndex = 23;
 			this.shouldRun.Text = "Yes";
 			// 
 			// label2
 			// 
-			this.label2.Location = new System.Drawing.Point(200, 48);
+			this.label2.Location = new System.Drawing.Point(167, 70);
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(104, 16);
+			this.label2.Size = new System.Drawing.Size(86, 13);
 			this.label2.TabIndex = 22;
 			this.label2.Text = "Runnable?";
 			this.label2.TextAlign = System.Drawing.ContentAlignment.TopRight;
@@ -227,16 +230,16 @@ namespace NUnit.UiKit
 			this.fullName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
 				| System.Windows.Forms.AnchorStyles.Right)));
 			this.fullName.CopySupported = true;
-			this.fullName.Location = new System.Drawing.Point(104, 16);
+			this.fullName.Location = new System.Drawing.Point(87, 14);
 			this.fullName.Name = "fullName";
-			this.fullName.Size = new System.Drawing.Size(296, 16);
+			this.fullName.Size = new System.Drawing.Size(244, 14);
 			this.fullName.TabIndex = 21;
 			// 
 			// label1
 			// 
-			this.label1.Location = new System.Drawing.Point(16, 16);
+			this.label1.Location = new System.Drawing.Point(13, 14);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(72, 16);
+			this.label1.Size = new System.Drawing.Size(67, 14);
 			this.label1.TabIndex = 20;
 			this.label1.Text = "Full Name:";
 			this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
@@ -245,22 +248,21 @@ namespace NUnit.UiKit
 			// 
 			this.resultsTab.Controls.Add(this.label3);
 			this.resultsTab.Controls.Add(this.testResult);
-			this.resultsTab.Controls.Add(this.leakage);
 			this.resultsTab.Controls.Add(this.stackTrace);
 			this.resultsTab.Controls.Add(this.label12);
 			this.resultsTab.Controls.Add(this.elapsedTime);
 			this.resultsTab.Controls.Add(this.message);
-			this.resultsTab.Location = new System.Drawing.Point(4, 25);
+			this.resultsTab.Location = new System.Drawing.Point(4, 22);
 			this.resultsTab.Name = "resultsTab";
-			this.resultsTab.Size = new System.Drawing.Size(408, 115);
+			this.resultsTab.Size = new System.Drawing.Size(338, 116);
 			this.resultsTab.TabIndex = 1;
 			this.resultsTab.Text = "Result";
 			// 
 			// label3
 			// 
-			this.label3.Location = new System.Drawing.Point(24, 48);
+			this.label3.Location = new System.Drawing.Point(20, 42);
 			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(64, 16);
+			this.label3.Size = new System.Drawing.Size(53, 13);
 			this.label3.TabIndex = 36;
 			this.label3.Text = "Message:";
 			this.label3.TextAlign = System.Drawing.ContentAlignment.TopRight;
@@ -268,20 +270,11 @@ namespace NUnit.UiKit
 			// testResult
 			// 
 			this.testResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((System.Byte)(0)));
-			this.testResult.Location = new System.Drawing.Point(24, 16);
+			this.testResult.Location = new System.Drawing.Point(20, 14);
 			this.testResult.Name = "testResult";
-			this.testResult.Size = new System.Drawing.Size(72, 16);
+			this.testResult.Size = new System.Drawing.Size(60, 14);
 			this.testResult.TabIndex = 35;
 			this.testResult.Text = "Failure";
-			// 
-			// leakage
-			// 
-			this.leakage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-			this.leakage.Location = new System.Drawing.Point(232, 16);
-			this.leakage.Name = "leakage";
-			this.leakage.Size = new System.Drawing.Size(168, 16);
-			this.leakage.TabIndex = 30;
-			this.leakage.Text = "Leakage:";
 			// 
 			// stackTrace
 			// 
@@ -290,26 +283,26 @@ namespace NUnit.UiKit
 				| System.Windows.Forms.AnchorStyles.Right)));
 			this.stackTrace.CopySupported = true;
 			this.stackTrace.Expansion = CP.Windows.Forms.TipWindow.ExpansionStyle.Both;
-			this.stackTrace.Location = new System.Drawing.Point(104, 80);
+			this.stackTrace.Location = new System.Drawing.Point(87, 69);
 			this.stackTrace.Name = "stackTrace";
 			this.stackTrace.Overlay = false;
-			this.stackTrace.Size = new System.Drawing.Size(296, 16);
+			this.stackTrace.Size = new System.Drawing.Size(244, 31);
 			this.stackTrace.TabIndex = 29;
 			// 
 			// label12
 			// 
-			this.label12.Location = new System.Drawing.Point(16, 80);
+			this.label12.Location = new System.Drawing.Point(13, 69);
 			this.label12.Name = "label12";
-			this.label12.Size = new System.Drawing.Size(72, 16);
+			this.label12.Size = new System.Drawing.Size(60, 14);
 			this.label12.TabIndex = 28;
 			this.label12.Text = "Stack:";
 			this.label12.TextAlign = System.Drawing.ContentAlignment.TopRight;
 			// 
 			// elapsedTime
 			// 
-			this.elapsedTime.Location = new System.Drawing.Point(104, 16);
+			this.elapsedTime.Location = new System.Drawing.Point(87, 14);
 			this.elapsedTime.Name = "elapsedTime";
-			this.elapsedTime.Size = new System.Drawing.Size(104, 16);
+			this.elapsedTime.Size = new System.Drawing.Size(86, 14);
 			this.elapsedTime.TabIndex = 27;
 			this.elapsedTime.Text = "Time:";
 			// 
@@ -319,26 +312,45 @@ namespace NUnit.UiKit
 				| System.Windows.Forms.AnchorStyles.Right)));
 			this.message.CopySupported = true;
 			this.message.Expansion = CP.Windows.Forms.TipWindow.ExpansionStyle.Both;
-			this.message.Location = new System.Drawing.Point(104, 48);
+			this.message.Location = new System.Drawing.Point(87, 42);
 			this.message.Name = "message";
-			this.message.Size = new System.Drawing.Size(288, 16);
+			this.message.Size = new System.Drawing.Size(238, 13);
 			this.message.TabIndex = 25;
 			// 
 			// pinButton
 			// 
 			this.pinButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
 			this.pinButton.Appearance = System.Windows.Forms.Appearance.Button;
-			this.pinButton.Location = new System.Drawing.Point(404, 8);
+			this.pinButton.Location = new System.Drawing.Point(337, 7);
 			this.pinButton.Name = "pinButton";
-			this.pinButton.Size = new System.Drawing.Size(20, 20);
+			this.pinButton.Size = new System.Drawing.Size(16, 17);
 			this.pinButton.TabIndex = 14;
 			this.pinButton.Click += new System.EventHandler(this.pinButton_Click);
 			this.pinButton.CheckedChanged += new System.EventHandler(this.pinButton_CheckedChanged);
 			// 
+			// label6
+			// 
+			this.label6.Location = new System.Drawing.Point(13, 42);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(67, 14);
+			this.label6.TabIndex = 28;
+			this.label6.Text = "Description:";
+			this.label6.TextAlign = System.Drawing.ContentAlignment.TopRight;
+			// 
+			// description
+			// 
+			this.description.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+				| System.Windows.Forms.AnchorStyles.Right)));
+			this.description.CopySupported = true;
+			this.description.Location = new System.Drawing.Point(87, 43);
+			this.description.Name = "description";
+			this.description.Size = new System.Drawing.Size(244, 14);
+			this.description.TabIndex = 29;
+			// 
 			// TestPropertiesDialog
 			// 
-			this.AutoScaleBaseSize = new System.Drawing.Size(6, 15);
-			this.ClientSize = new System.Drawing.Size(434, 154);
+			this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
+			this.ClientSize = new System.Drawing.Size(361, 150);
 			this.Controls.Add(this.pinButton);
 			this.Controls.Add(this.tabControl1);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
@@ -428,6 +440,7 @@ namespace NUnit.UiKit
 			// Initialize Test Tab
 			fullName.Text = test.FullName;
 			shouldRun.Text = test.ShouldRun ? "Yes" : "No";
+			description.Text = test.Description;
 			ignoreReason.Text = test.IgnoreReason;
 			testCaseCount.Text = test.CountTestCases().ToString();
 
@@ -447,11 +460,6 @@ namespace NUnit.UiKit
 				message.Text = TrimLeadingBlankLines( result.Message );
 				elapsedTime.Text = string.Format( "Time: {0}", result.Time );
 				stackTrace.Text = result.StackTrace;
-#if NUNIT_LEAKAGE_TEST
-				leakage.Text = string.Format( "Leakage: {0} bytes", result.Leakage );
-#else
-				leakage.Text = string.Empty;
-#endif
 			}
 		}
 
