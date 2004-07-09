2bf13585f0024c6365772208ec6fd3877dfe4c30
charliepoole <>
2004-07-09 16:58:00 +0000

    Update version numbers for Mono Beta 2 release

--- src/tests/mock-assembly/AssemblyInfo.cs
+++ src/tests/mock-assembly/AssemblyInfo.cs
@@ -26,7 +26,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("2.1.92.0")]
+[assembly: AssemblyVersion("2.1.92.1")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
@@ -53,9 +53,6 @@ using System.Runtime.CompilerServices;
 //   (*) Delay Signing is an advanced option - see the Microsoft .NET Framework
 //       documentation for more information on this.
 //
-#if !StronglyNamedAssembly
 [assembly: AssemblyDelaySign(false)]
-#else
-[assembly: AssemblyKeyFile("..\\..\\..\\..\\Nunit.key")]
+[assembly: AssemblyKeyFile("")]
 [assembly: AssemblyKeyName("")]
-#endif
