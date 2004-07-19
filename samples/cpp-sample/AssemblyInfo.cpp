223190fb3143615864c96745b0297f46f35b89c0
charliepoole <>
2004-07-19 19:41:37 +0000

    Increment version to 2.1.93

--- samples/cpp-sample/AssemblyInfo.cpp
+++ samples/cpp-sample/AssemblyInfo.cpp
@@ -28,7 +28,7 @@ using namespace System::Runtime::CompilerServices;
 // You can specify all the value or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly:AssemblyVersionAttribute("2.1.92.1")];
+[assembly:AssemblyVersionAttribute("2.1.93.0")];
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
