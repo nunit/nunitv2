da7262f7e84400654372bc6d1a6b93da90f3d88a
jcansdale <>
2004-10-11 00:42:24 +0000

    Automated build file.

--- src/install/build.bat
+++ src/install/build.bat
@@ -1,3 +1,1 @@
-..\..\tools\mutantbuild\mutantbuild @debug(1.0).response /property:Configuration=Release /property:OutputPath=%cd%\bin\ /target:Clean;Rebuild ..\nunit.sln
-..\..\tools\wix\candle NUnit.wxs UI.wxi bin.wxi doc.wxi src.wxi samples.wxi
-..\..\tools\wix\light NUnit.wixobj UI.wixobj bin.wixobj doc.wixobj src.wixobj samples.wixobj -out NUnit-2.2.1.msi
+..\..\tools\mutantbuild\mutantbuild NUnit.proj
