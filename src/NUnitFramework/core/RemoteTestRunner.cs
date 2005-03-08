a1a86167997259d7a0076286ee04151f819d160e
charliepoole <>
2005-03-08 18:59:35 +0000

    Eliminate use of ThreadedTestRunner here; create EventListenerTextWriter and EventPump; use TestContext to save console state.

--- src/NUnitFramework/core/RemoteTestRunner.cs
+++ src/NUnitFramework/core/RemoteTestRunner.cs
@@ -4,19 +4,39 @@ namespace NUnit.Core
 
 	public class RemoteTestRunner : ProxyTestRunner
 	{
+		#region Constructor
 		public RemoteTestRunner() 
-			: base( new ThreadedTestRunner(new RealTestRunner() ) ) {	}
+			: base( new BaseTestRunner() ) { }
+		#endregion
 
-		public override void StartRun(EventListener listener)
+		#region Method Overrides
+		public override TestResult[] doRun( EventListener listener, string[] testNames )
 		{
-			base.StartRun (listener);
+			QueuingEventListener queue = new QueuingEventListener();
+
+			this.Out = new EventListenerTextWriter( queue, TestOutputType.Out );
+			this.Error = new EventListenerTextWriter( queue, TestOutputType.Error );
+
+			using( EventPump pump = new EventPump( listener, queue.Events, true ) )
+			{
+				pump.Start();
+				return base.doRun( queue, testNames );
+			}
 		}
 
-		public override void StartRun(EventListener listener, string[] testNames)
+		public override void doStartRun( EventListener listener, string[] testNames )
 		{
-			base.StartRun (listener, testNames);
+			QueuingEventListener queue = new QueuingEventListener();
+
+			this.Out = new EventListenerTextWriter( queue, TestOutputType.Out );
+			this.Error = new EventListenerTextWriter( queue, TestOutputType.Error );
+
+			EventPump pump = new EventPump( listener, queue.Events, true);
+			pump.Start(); // Will run till RunFinished is received
+			// TODO: Make sure the thread is cleaned up if we abort the run
+			
+			base.doStartRun( queue, testNames );
 		}
-
-
+		#endregion
 	}
 }
