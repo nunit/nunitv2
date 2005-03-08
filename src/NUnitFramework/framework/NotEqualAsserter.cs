4ede02f079ee8fa6e5ac2eb4a4a88c191b663b9e
charliepoole <>
2005-03-08 17:16:10 +0000

    Add documentation comments

--- src/NUnitFramework/framework/NotEqualAsserter.cs
+++ src/NUnitFramework/framework/NotEqualAsserter.cs
@@ -3,13 +3,25 @@ using System;
 namespace NUnit.Framework
 {
 	/// <summary>
-	/// Summary description for NotEqualAsserter.
+	/// NotEqualAsserter is the asserter class that handles 
+	/// inequality assertions.
 	/// </summary>
 	public class NotEqualAsserter : EqualityAsserter
 	{
+		/// <summary>
+		/// Constructor for NotEqualAsserter
+		/// </summary>
+		/// <param name="expected">The expected object</param>
+		/// <param name="actual">The actual object</param>
+		/// <param name="message">The message to be printed when the two objects are the same object.</param>
+		/// <param name="args">Arguments to be used in formatting the message</param>
 		public NotEqualAsserter( object expected, object actual, string message, params object[] args )
 			: base( expected, actual, message, args ) { }
 
+		/// <summary>
+		/// Assert that the two objects are not equal, failing
+		/// if they are found to be equal.
+		/// </summary>
 		public override void Assert()
 		{
 			if ( expected == null && actual == null ) Fail();
@@ -24,6 +36,11 @@ namespace NUnit.Framework
 				Fail();
 		}
 
+		/// <summary>
+		/// Fail by throwing an AssertionException with the message 
+		/// provided by the user.
+		/// </summary>
+		/// <returns></returns>
 		public bool Fail()
 		{
 			throw new AssertionException( FormattedMessage );
