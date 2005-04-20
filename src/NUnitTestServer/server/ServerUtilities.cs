92034a8f34f52924226f6339cdb82f7f13fe8f83
jcansdale <>
2005-04-20 17:26:23 +0000

    Use reflection to keep this working with .NET 1.0.

--- src/NUnitTestServer/server/ServerUtilities.cs
+++ src/NUnitTestServer/server/ServerUtilities.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Runtime.Remoting;
 using System.Runtime.Remoting.Channels;
 using System.Runtime.Remoting.Channels.Tcp;
+using System.Reflection;
 
 namespace NUnit.TestServer
 {
@@ -11,16 +12,27 @@ namespace NUnit.TestServer
 	/// </summary>
 	public class ServerUtilities
 	{
-		public static TcpChannel GetTcpChannel( IDictionary properties )
+		public static TcpChannel GetTcpChannel(IDictionary properties)
 		{
 			BinaryServerFormatterSinkProvider serverProvider =
 				new BinaryServerFormatterSinkProvider();
-			serverProvider.TypeFilterLevel = 
-				System.Runtime.Serialization.Formatters.TypeFilterLevel.Full;
+
+			// NOTE: Use reflection in order to stay compatabe with .NET 1.0.
+			// serverProvider.TypeFilterLevel =
+			//    System.Runtime.Serialization.Formatters.TypeFilterLevel.Full;
+			Type typeFilterLevelType = Type.GetType("System.Runtime.Serialization.Formatters.TypeFilterLevel");
+			if (typeFilterLevelType != null)
+			{
+				object typeFilterLevel = Enum.Parse(typeFilterLevelType, "Full");
+				PropertyInfo typeFilterLevelProperty = serverProvider.GetType().GetProperty("TypeFilterLevel");
+				typeFilterLevelProperty.SetValue(serverProvider, typeFilterLevel, null);
+			}
+
 			BinaryClientFormatterSinkProvider clientProvider =
 				new BinaryClientFormatterSinkProvider();
 
-			return new TcpChannel( properties, clientProvider, serverProvider );
+			return new TcpChannel(properties, clientProvider, serverProvider);
 		}
+
 	}
 }
