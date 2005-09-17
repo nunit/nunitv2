4fdd829e8fb8e23405f0a153f414c5d50e78f3ef
jcansdale <>
2005-09-17 17:14:54 +0000

    Build script and environment to target .NET 1.0 using MSBuild/.NET2.0 RC.

---
+++ src/vars.bat
@@ -0,0 +1,5 @@
+@echo off
+@echo This build environment requires .NET Framework 2.0 RC (v2.0.50727)
+@echo To build use 'msbuild NUnit.proj'
+
+set path=%path%;%SystemRoot%\Microsoft.NET\Framework\v2.0.50727
