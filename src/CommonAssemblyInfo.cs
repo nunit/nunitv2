c1a3014dfbbb67ca4649fee19a33d9fab507c0d9
charliepoole <>
2005-11-30 03:03:05 +0000

    Set common version to 2.3.5332

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -54,4 +54,4 @@ using System.Reflection;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("2.4.1.0")]
+[assembly: AssemblyVersion("2.3.5332.0")]
