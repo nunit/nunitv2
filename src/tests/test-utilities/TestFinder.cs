8907d208ac0220b9d6baa48203a380ee9529c058
charliepoole <>
2006-03-06 22:37:30 +0000

    Add new TestFinder utility class

---
+++ src/tests/test-utilities/TestFinder.cs
@@ -0,0 +1,50 @@
+using System;
+using NUnit.Core;
+
+namespace NUnit.TestUtilities
+{
+	/// <summary>
+	/// Summary description for TestFinder.
+	/// </summary>
+	public class TestFinder
+	{
+		public static Test Find(string name, Test test)
+		{
+			Test result = null;
+			if (test.Name == name)
+				result = test;
+			else if (test.Tests != null)
+			{
+				foreach(Test t in test.Tests) 
+				{
+					result = Find(name, t);
+					if (result != null)
+						break;
+				}
+			}
+
+			return result;
+		}
+		
+		public static TestResult Find(string name, TestResult result) 
+		{
+			if (result.Test.Name == name)
+				return result;
+
+			TestSuiteResult suiteResult = result as TestSuiteResult;
+			if ( suiteResult != null )
+			{
+				foreach( TestResult r in suiteResult.Results ) 
+				{
+					TestResult myResult = Find( name, r );
+					if ( myResult != null )
+						return myResult;
+				}
+			}
+
+			return null;
+		}
+
+		private TestFinder() { }
+	}
+}
