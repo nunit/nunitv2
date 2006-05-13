207d324f10eae6e727e686b0112fb7daa10394dc
charliepoole <>
2006-05-13 00:07:49 +0000

    Make extensions assemblies strong-named

---
+++ src/NUnitExtensions/core/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System;
+using System.Reflection;
+
+[assembly: CLSCompliant(true)]
+
+[assembly: AssemblyDelaySign(false)]
+[assembly: AssemblyKeyFile("../../../../nunit.snk")]
+[assembly: AssemblyKeyName("")]
