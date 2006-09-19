0a7e9358872446b6b32cf3bcf89a72ca63569c06
jcansdale <>
2006-09-19 22:59:26 +0000

    Removed installer building targets from MSBuild script.

--- src/vars.bat
+++ src/vars.bat
@@ -1,5 +1,5 @@
 @echo off
 @echo This build environment requires .NET Framework 2.0 RTM (v2.0.50727)
-@echo To build use 'msbuild NUnit.proj /p:version=x.x.x'
+@echo To build use 'msbuild NUnit.proj'
 
 set path=%path%;%SystemRoot%\Microsoft.NET\Framework\v2.0.50727
