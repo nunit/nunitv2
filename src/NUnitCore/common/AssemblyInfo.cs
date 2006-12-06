10fd9f70b26178b3f82866364c919c368b33223b
charliepoole <>
2006-12-06 00:53:20 +0000

    Remove nunit.common project from the solution.

--- src/NUnitCore/common/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-using System;
-using System.Reflection;
-
-[assembly: CLSCompliant(true)]
-
-[assembly: AssemblyDelaySign(false)]
-[assembly: AssemblyKeyFile("../../../../nunit.snk")]
-[assembly: AssemblyKeyName("")]
