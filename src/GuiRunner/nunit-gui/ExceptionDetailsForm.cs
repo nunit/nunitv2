aa017416427954eda3f95e82a97a3ab2484c333f
charliepoole <>
2006-12-29 20:59:20 +0000

    Remove MS-style localization

--- src/GuiRunner/nunit-gui/ExceptionDetailsForm.cs
+++ src/GuiRunner/nunit-gui/ExceptionDetailsForm.cs
@@ -56,7 +56,6 @@ namespace NUnit.Gui
 		/// </summary>
 		private void InitializeComponent()
 		{
-			System.Resources.ResourceManager resources = new System.Resources.ResourceManager(typeof(ExceptionDetailsForm));
 			this.okButton = new System.Windows.Forms.Button();
 			this.stackTrace = new System.Windows.Forms.RichTextBox();
 			this.message = new System.Windows.Forms.Label();
@@ -64,101 +63,46 @@ namespace NUnit.Gui
 			// 
 			// okButton
 			// 
-			this.okButton.AccessibleDescription = resources.GetString("okButton.AccessibleDescription");
-			this.okButton.AccessibleName = resources.GetString("okButton.AccessibleName");
-			this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)(resources.GetObject("okButton.Anchor")));
-			this.okButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("okButton.BackgroundImage")));
+			this.okButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
 			this.okButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.okButton.Dock = ((System.Windows.Forms.DockStyle)(resources.GetObject("okButton.Dock")));
-			this.okButton.Enabled = ((bool)(resources.GetObject("okButton.Enabled")));
-			this.okButton.FlatStyle = ((System.Windows.Forms.FlatStyle)(resources.GetObject("okButton.FlatStyle")));
-			this.okButton.Font = ((System.Drawing.Font)(resources.GetObject("okButton.Font")));
-			this.okButton.Image = ((System.Drawing.Image)(resources.GetObject("okButton.Image")));
-			this.okButton.ImageAlign = ((System.Drawing.ContentAlignment)(resources.GetObject("okButton.ImageAlign")));
-			this.okButton.ImageIndex = ((int)(resources.GetObject("okButton.ImageIndex")));
-			this.okButton.ImeMode = ((System.Windows.Forms.ImeMode)(resources.GetObject("okButton.ImeMode")));
-			this.okButton.Location = ((System.Drawing.Point)(resources.GetObject("okButton.Location")));
+			this.okButton.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+			this.okButton.Location = new System.Drawing.Point(434, 512);
 			this.okButton.Name = "okButton";
-			this.okButton.RightToLeft = ((System.Windows.Forms.RightToLeft)(resources.GetObject("okButton.RightToLeft")));
-			this.okButton.Size = ((System.Drawing.Size)(resources.GetObject("okButton.Size")));
-			this.okButton.TabIndex = ((int)(resources.GetObject("okButton.TabIndex")));
-			this.okButton.Text = resources.GetString("okButton.Text");
-			this.okButton.TextAlign = ((System.Drawing.ContentAlignment)(resources.GetObject("okButton.TextAlign")));
-			this.okButton.Visible = ((bool)(resources.GetObject("okButton.Visible")));
+			this.okButton.Size = new System.Drawing.Size(90, 27);
+			this.okButton.TabIndex = 1;
+			this.okButton.Text = "OK";
 			// 
 			// stackTrace
 			// 
-			this.stackTrace.AccessibleDescription = resources.GetString("stackTrace.AccessibleDescription");
-			this.stackTrace.AccessibleName = resources.GetString("stackTrace.AccessibleName");
-			this.stackTrace.Anchor = ((System.Windows.Forms.AnchorStyles)(resources.GetObject("stackTrace.Anchor")));
-			this.stackTrace.AutoSize = ((bool)(resources.GetObject("stackTrace.AutoSize")));
-			this.stackTrace.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("stackTrace.BackgroundImage")));
-			this.stackTrace.BulletIndent = ((int)(resources.GetObject("stackTrace.BulletIndent")));
-			this.stackTrace.Dock = ((System.Windows.Forms.DockStyle)(resources.GetObject("stackTrace.Dock")));
-			this.stackTrace.Enabled = ((bool)(resources.GetObject("stackTrace.Enabled")));
-			this.stackTrace.Font = ((System.Drawing.Font)(resources.GetObject("stackTrace.Font")));
-			this.stackTrace.ImeMode = ((System.Windows.Forms.ImeMode)(resources.GetObject("stackTrace.ImeMode")));
-			this.stackTrace.Location = ((System.Drawing.Point)(resources.GetObject("stackTrace.Location")));
-			this.stackTrace.MaxLength = ((int)(resources.GetObject("stackTrace.MaxLength")));
-			this.stackTrace.Multiline = ((bool)(resources.GetObject("stackTrace.Multiline")));
+			this.stackTrace.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+				| System.Windows.Forms.AnchorStyles.Left) 
+				| System.Windows.Forms.AnchorStyles.Right)));
+			this.stackTrace.Location = new System.Drawing.Point(10, 65);
 			this.stackTrace.Name = "stackTrace";
 			this.stackTrace.ReadOnly = true;
-			this.stackTrace.RightMargin = ((int)(resources.GetObject("stackTrace.RightMargin")));
-			this.stackTrace.RightToLeft = ((System.Windows.Forms.RightToLeft)(resources.GetObject("stackTrace.RightToLeft")));
-			this.stackTrace.ScrollBars = ((System.Windows.Forms.RichTextBoxScrollBars)(resources.GetObject("stackTrace.ScrollBars")));
-			this.stackTrace.Size = ((System.Drawing.Size)(resources.GetObject("stackTrace.Size")));
-			this.stackTrace.TabIndex = ((int)(resources.GetObject("stackTrace.TabIndex")));
-			this.stackTrace.Text = resources.GetString("stackTrace.Text");
-			this.stackTrace.Visible = ((bool)(resources.GetObject("stackTrace.Visible")));
-			this.stackTrace.WordWrap = ((bool)(resources.GetObject("stackTrace.WordWrap")));
-			this.stackTrace.ZoomFactor = ((System.Single)(resources.GetObject("stackTrace.ZoomFactor")));
+			this.stackTrace.Size = new System.Drawing.Size(940, 433);
+			this.stackTrace.TabIndex = 3;
+			this.stackTrace.Text = "";
 			// 
 			// message
 			// 
-			this.message.AccessibleDescription = resources.GetString("message.AccessibleDescription");
-			this.message.AccessibleName = resources.GetString("message.AccessibleName");
-			this.message.Anchor = ((System.Windows.Forms.AnchorStyles)(resources.GetObject("message.Anchor")));
-			this.message.AutoSize = ((bool)(resources.GetObject("message.AutoSize")));
-			this.message.Dock = ((System.Windows.Forms.DockStyle)(resources.GetObject("message.Dock")));
-			this.message.Enabled = ((bool)(resources.GetObject("message.Enabled")));
-			this.message.Font = ((System.Drawing.Font)(resources.GetObject("message.Font")));
-			this.message.Image = ((System.Drawing.Image)(resources.GetObject("message.Image")));
-			this.message.ImageAlign = ((System.Drawing.ContentAlignment)(resources.GetObject("message.ImageAlign")));
-			this.message.ImageIndex = ((int)(resources.GetObject("message.ImageIndex")));
-			this.message.ImeMode = ((System.Windows.Forms.ImeMode)(resources.GetObject("message.ImeMode")));
-			this.message.Location = ((System.Drawing.Point)(resources.GetObject("message.Location")));
+			this.message.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+				| System.Windows.Forms.AnchorStyles.Right)));
+			this.message.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+			this.message.Location = new System.Drawing.Point(19, 9);
 			this.message.Name = "message";
-			this.message.RightToLeft = ((System.Windows.Forms.RightToLeft)(resources.GetObject("message.RightToLeft")));
-			this.message.Size = ((System.Drawing.Size)(resources.GetObject("message.Size")));
-			this.message.TabIndex = ((int)(resources.GetObject("message.TabIndex")));
-			this.message.Text = resources.GetString("message.Text");
-			this.message.TextAlign = ((System.Drawing.ContentAlignment)(resources.GetObject("message.TextAlign")));
-			this.message.Visible = ((bool)(resources.GetObject("message.Visible")));
+			this.message.Size = new System.Drawing.Size(931, 46);
+			this.message.TabIndex = 2;
 			// 
 			// ExceptionDetailsForm
 			// 
-			this.AccessibleDescription = resources.GetString("$this.AccessibleDescription");
-			this.AccessibleName = resources.GetString("$this.AccessibleName");
-			this.AutoScaleBaseSize = ((System.Drawing.Size)(resources.GetObject("$this.AutoScaleBaseSize")));
-			this.AutoScroll = ((bool)(resources.GetObject("$this.AutoScroll")));
-			this.AutoScrollMargin = ((System.Drawing.Size)(resources.GetObject("$this.AutoScrollMargin")));
-			this.AutoScrollMinSize = ((System.Drawing.Size)(resources.GetObject("$this.AutoScrollMinSize")));
-			this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-			this.ClientSize = ((System.Drawing.Size)(resources.GetObject("$this.ClientSize")));
+			this.AutoScaleBaseSize = new System.Drawing.Size(6, 15);
+			this.ClientSize = new System.Drawing.Size(969, 551);
 			this.Controls.Add(this.stackTrace);
 			this.Controls.Add(this.message);
 			this.Controls.Add(this.okButton);
-			this.Enabled = ((bool)(resources.GetObject("$this.Enabled")));
-			this.Font = ((System.Drawing.Font)(resources.GetObject("$this.Font")));
-			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-			this.ImeMode = ((System.Windows.Forms.ImeMode)(resources.GetObject("$this.ImeMode")));
-			this.Location = ((System.Drawing.Point)(resources.GetObject("$this.Location")));
-			this.MaximumSize = ((System.Drawing.Size)(resources.GetObject("$this.MaximumSize")));
-			this.MinimumSize = ((System.Drawing.Size)(resources.GetObject("$this.MinimumSize")));
 			this.Name = "ExceptionDetailsForm";
-			this.RightToLeft = ((System.Windows.Forms.RightToLeft)(resources.GetObject("$this.RightToLeft")));
-			this.StartPosition = ((System.Windows.Forms.FormStartPosition)(resources.GetObject("$this.StartPosition")));
-			this.Text = resources.GetString("$this.Text");
+			this.Text = "Exception Details";
 			this.Resize += new System.EventHandler(this.ExceptionDetailsForm_Resize);
 			this.Load += new System.EventHandler(this.ExceptionDetailsForm_Load);
 			this.ResumeLayout(false);
@@ -168,6 +112,7 @@ namespace NUnit.Gui
 
 		private void ExceptionDetailsForm_Load(object sender, System.EventArgs e)
 		{
+			//TODO: Put some of this in the constructor for easier testing?
 			this.message.Text = FormatMessage( exception );
 			SetMessageLabelSize();
 
