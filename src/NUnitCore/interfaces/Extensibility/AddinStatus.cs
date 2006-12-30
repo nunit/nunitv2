ec65999a807d5ae2284748feef47d1f2f57e6c3e
charliepoole <>
2006-12-30 21:47:29 +0000

    Add missing file

---
+++ src/NUnitCore/interfaces/Extensibility/AddinStatus.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace NUnit.Core.Extensibility
+{
+    public enum AddinStatus
+    {
+        Unknown,
+        Enabled,
+        Disabled,
+        Loaded,
+        Error
+    }
+}
