bc1f3d25174a643c1b43196bcd2e1760e595f7e4
charliepoole <>
2007-07-11 18:25:59 +0000

    Layout adjustments; display confirmation dialog before removing an assembly from the project.

--- src/GuiRunner/nunit-gui/ProjectEditor.cs
+++ src/GuiRunner/nunit-gui/ProjectEditor.cs
@@ -38,7 +38,6 @@ namespace NUnit.Gui
 		private System.Windows.Forms.Button editAssemblyButton;
 		private System.Windows.Forms.Button addAssemblyButton;
 		private System.Windows.Forms.Button deleteAssemblyButton;
-		private System.Windows.Forms.Label label2;
 		private System.Windows.Forms.Label label3;
 		private System.Windows.Forms.TextBox applicationBaseTextBox;
 		private System.Windows.Forms.Label label4;
@@ -122,7 +121,6 @@ namespace NUnit.Gui
 			this.editAssemblyButton = new System.Windows.Forms.Button();
 			this.addAssemblyButton = new System.Windows.Forms.Button();
 			this.deleteAssemblyButton = new System.Windows.Forms.Button();
-			this.label2 = new System.Windows.Forms.Label();
 			this.fileNameHeader = new System.Windows.Forms.ColumnHeader();
 			this.fullPathHeader = new System.Windows.Forms.ColumnHeader();
 			this.closeButton = new System.Windows.Forms.Button();
@@ -141,7 +139,7 @@ namespace NUnit.Gui
 			this.editConfigsButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
 			this.helpProvider1.SetHelpString(this.editConfigsButton, "Add, remove or rename configurations.");
 			this.editConfigsButton.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-			this.editConfigsButton.Location = new System.Drawing.Point(424, 92);
+			this.editConfigsButton.Location = new System.Drawing.Point(424, 72);
 			this.editConfigsButton.Name = "editConfigsButton";
 			this.helpProvider1.SetShowHelp(this.editConfigsButton, true);
 			this.editConfigsButton.Size = new System.Drawing.Size(104, 23);
@@ -155,7 +153,7 @@ namespace NUnit.Gui
 				| System.Windows.Forms.AnchorStyles.Right)));
 			this.helpProvider1.SetHelpString(this.configComboBox, "Select the configuration to edit");
 			this.configComboBox.ItemHeight = 16;
-			this.configComboBox.Location = new System.Drawing.Point(125, 92);
+			this.configComboBox.Location = new System.Drawing.Point(125, 72);
 			this.configComboBox.Name = "configComboBox";
 			this.helpProvider1.SetShowHelp(this.configComboBox, true);
 			this.configComboBox.Size = new System.Drawing.Size(288, 24);
@@ -165,7 +163,7 @@ namespace NUnit.Gui
 			// label1
 			// 
 			this.label1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-			this.label1.Location = new System.Drawing.Point(23, 92);
+			this.label1.Location = new System.Drawing.Point(23, 72);
 			this.label1.Name = "label1";
 			this.helpProvider1.SetShowHelp(this.label1, false);
 			this.label1.Size = new System.Drawing.Size(102, 16);
@@ -180,11 +178,11 @@ namespace NUnit.Gui
 			this.projectTabControl.Controls.Add(this.generalTabPage);
 			this.projectTabControl.Controls.Add(this.assemblyTabPage);
 			this.projectTabControl.ItemSize = new System.Drawing.Size(49, 18);
-			this.projectTabControl.Location = new System.Drawing.Point(16, 129);
+			this.projectTabControl.Location = new System.Drawing.Point(16, 112);
 			this.projectTabControl.Name = "projectTabControl";
 			this.projectTabControl.SelectedIndex = 0;
 			this.helpProvider1.SetShowHelp(this.projectTabControl, false);
-			this.projectTabControl.Size = new System.Drawing.Size(512, 266);
+			this.projectTabControl.Size = new System.Drawing.Size(512, 257);
 			this.projectTabControl.TabIndex = 5;
 			// 
 			// generalTabPage
@@ -203,7 +201,7 @@ namespace NUnit.Gui
 			this.generalTabPage.Location = new System.Drawing.Point(4, 22);
 			this.generalTabPage.Name = "generalTabPage";
 			this.helpProvider1.SetShowHelp(this.generalTabPage, false);
-			this.generalTabPage.Size = new System.Drawing.Size(504, 240);
+			this.generalTabPage.Size = new System.Drawing.Size(504, 231);
 			this.generalTabPage.TabIndex = 0;
 			this.generalTabPage.Text = "General";
 			// 
@@ -348,11 +346,10 @@ namespace NUnit.Gui
 			this.assemblyTabPage.Controls.Add(this.editAssemblyButton);
 			this.assemblyTabPage.Controls.Add(this.addAssemblyButton);
 			this.assemblyTabPage.Controls.Add(this.deleteAssemblyButton);
-			this.assemblyTabPage.Controls.Add(this.label2);
 			this.assemblyTabPage.Location = new System.Drawing.Point(4, 22);
 			this.assemblyTabPage.Name = "assemblyTabPage";
 			this.helpProvider1.SetShowHelp(this.assemblyTabPage, false);
-			this.assemblyTabPage.Size = new System.Drawing.Size(504, 240);
+			this.assemblyTabPage.Size = new System.Drawing.Size(504, 231);
 			this.assemblyTabPage.TabIndex = 1;
 			this.assemblyTabPage.Text = "Assemblies";
 			// 
@@ -361,10 +358,11 @@ namespace NUnit.Gui
 			this.helpProvider1.SetHelpString(this.assemblyListBox, "Checked assemblies will have tests loaded in the UI. Tests (if any) in unchecked " +
 				"assemblies will not be loaded. All listed assemblies are watched for changes and" +
 				" used in determining the PrivateBinPath.");
-			this.assemblyListBox.Location = new System.Drawing.Point(24, 32);
+			this.assemblyListBox.ItemHeight = 16;
+			this.assemblyListBox.Location = new System.Drawing.Point(24, 16);
 			this.assemblyListBox.Name = "assemblyListBox";
 			this.helpProvider1.SetShowHelp(this.assemblyListBox, true);
-			this.assemblyListBox.Size = new System.Drawing.Size(328, 157);
+			this.assemblyListBox.Size = new System.Drawing.Size(328, 180);
 			this.assemblyListBox.TabIndex = 6;
 			this.assemblyListBox.SelectedIndexChanged += new System.EventHandler(this.assemblyListBox_SelectedIndexChanged);
 			// 
@@ -374,7 +372,7 @@ namespace NUnit.Gui
 			this.helpProvider1.SetHelpString(this.addVSProjectButton, "Add the output assembly from a VS project to this project. Assembly will be added" +
 				" to each configuration that exists in the VS project.");
 			this.addVSProjectButton.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-			this.addVSProjectButton.Location = new System.Drawing.Point(369, 72);
+			this.addVSProjectButton.Location = new System.Drawing.Point(369, 56);
 			this.addVSProjectButton.Name = "addVSProjectButton";
 			this.helpProvider1.SetShowHelp(this.addVSProjectButton, true);
 			this.addVSProjectButton.Size = new System.Drawing.Size(128, 23);
@@ -387,7 +385,7 @@ namespace NUnit.Gui
 			this.editAssemblyButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
 			this.helpProvider1.SetHelpString(this.editAssemblyButton, "Modify the path to the selected assembly.");
 			this.editAssemblyButton.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-			this.editAssemblyButton.Location = new System.Drawing.Point(369, 104);
+			this.editAssemblyButton.Location = new System.Drawing.Point(369, 88);
 			this.editAssemblyButton.Name = "editAssemblyButton";
 			this.helpProvider1.SetShowHelp(this.editAssemblyButton, true);
 			this.editAssemblyButton.Size = new System.Drawing.Size(128, 23);
@@ -400,7 +398,7 @@ namespace NUnit.Gui
 			this.addAssemblyButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
 			this.helpProvider1.SetHelpString(this.addAssemblyButton, "Add an assembly to this configuration.");
 			this.addAssemblyButton.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-			this.addAssemblyButton.Location = new System.Drawing.Point(369, 40);
+			this.addAssemblyButton.Location = new System.Drawing.Point(369, 24);
 			this.addAssemblyButton.Name = "addAssemblyButton";
 			this.helpProvider1.SetShowHelp(this.addAssemblyButton, true);
 			this.addAssemblyButton.Size = new System.Drawing.Size(128, 23);
@@ -413,23 +411,13 @@ namespace NUnit.Gui
 			this.deleteAssemblyButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
 			this.helpProvider1.SetHelpString(this.deleteAssemblyButton, "Remove the selected assembly from the configuration.");
 			this.deleteAssemblyButton.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-			this.deleteAssemblyButton.Location = new System.Drawing.Point(369, 136);
+			this.deleteAssemblyButton.Location = new System.Drawing.Point(369, 120);
 			this.deleteAssemblyButton.Name = "deleteAssemblyButton";
 			this.helpProvider1.SetShowHelp(this.deleteAssemblyButton, true);
 			this.deleteAssemblyButton.Size = new System.Drawing.Size(128, 23);
 			this.deleteAssemblyButton.TabIndex = 5;
 			this.deleteAssemblyButton.Text = "&Remove Assembly";
 			this.deleteAssemblyButton.Click += new System.EventHandler(this.deleteAssemblyButton_Click);
-			// 
-			// label2
-			// 
-			this.label2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-			this.label2.Location = new System.Drawing.Point(16, 8);
-			this.label2.Name = "label2";
-			this.helpProvider1.SetShowHelp(this.label2, false);
-			this.label2.Size = new System.Drawing.Size(464, 16);
-			this.label2.TabIndex = 0;
-			this.label2.Text = "Indicate assemblies with tests by checking the box.";
 			// 
 			// fileNameHeader
 			// 
@@ -446,7 +434,7 @@ namespace NUnit.Gui
 			this.closeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.closeButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
 			this.closeButton.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-			this.closeButton.Location = new System.Drawing.Point(424, 406);
+			this.closeButton.Location = new System.Drawing.Point(424, 383);
 			this.closeButton.Name = "closeButton";
 			this.helpProvider1.SetShowHelp(this.closeButton, false);
 			this.closeButton.Size = new System.Drawing.Size(104, 23);
@@ -503,7 +491,7 @@ namespace NUnit.Gui
 			this.AcceptButton = this.closeButton;
 			this.AutoScaleBaseSize = new System.Drawing.Size(6, 15);
 			this.CancelButton = this.closeButton;
-			this.ClientSize = new System.Drawing.Size(537, 433);
+			this.ClientSize = new System.Drawing.Size(537, 410);
 			this.Controls.Add(this.projectBaseTextBox);
 			this.Controls.Add(this.label8);
 			this.Controls.Add(this.projectPathLabel);
@@ -516,7 +504,6 @@ namespace NUnit.Gui
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
 			this.HelpButton = true;
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-			this.ImeMode = System.Windows.Forms.ImeMode.NoControl;
 			this.MaximizeBox = false;
 			this.MinimizeBox = false;
 			this.MinimumSize = new System.Drawing.Size(376, 400);
@@ -894,19 +881,23 @@ namespace NUnit.Gui
 
 		private void deleteAssemblyButton_Click(object sender, System.EventArgs e)
 		{
-			int index = assemblyListBox.SelectedIndex;
-			selectedConfig.Assemblies.RemoveAt( index );
-			assemblyListBox.Items.RemoveAt( index );
-			if ( index >= assemblyListBox.Items.Count )
-				--index;
-
-			if ( index >= 0 )
+			if ( UserMessage.Ask( string.Format(
+				"Remove {0} from project?", selectedAssembly ) ) == DialogResult.Yes )
 			{
-				selectedAssembly = (string)assemblyListBox.Items[index];
-				assemblyListBox.SelectedIndex = index;
+				int index = assemblyListBox.SelectedIndex;
+				selectedConfig.Assemblies.RemoveAt( index );
+				assemblyListBox.Items.RemoveAt( index );
+				if ( index >= assemblyListBox.Items.Count )
+					--index;
+
+				if ( index >= 0 )
+				{
+					selectedAssembly = (string)assemblyListBox.Items[index];
+					assemblyListBox.SelectedIndex = index;
+				}
+				else
+					selectedAssembly = null;
 			}
-			else
-				selectedAssembly = null;
 		}
 
 		private void closeButton_Click(object sender, System.EventArgs e)
