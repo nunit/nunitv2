2ee825f67ab5ad614e1193c066e3525a070f07c1
charliepoole <>
2007-07-13 22:19:13 +0000

    Add attribute usage to SetCultureAttribute - Jamie's build is not picking up the usage from the base class... .Net 1.0 issue?

--- src/NUnitFramework/framework/SetCultureAttribute.cs
+++ src/NUnitFramework/framework/SetCultureAttribute.cs
@@ -5,6 +5,7 @@ namespace NUnit.Framework
 	/// <summary>
 	/// Summary description for SetCultureAttribute.
 	/// </summary>
+	[AttributeUsage(AttributeTargets.Class|AttributeTargets.Method|AttributeTargets.Assembly, AllowMultiple=true)]
 	public class SetCultureAttribute : PropertyAttribute
 	{
 		/// <summary>
