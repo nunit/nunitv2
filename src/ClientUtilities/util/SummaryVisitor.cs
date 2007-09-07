f90f1bd079ec5f3c069ec36ebfb980f9991a5b2a
charliepoole <>
2007-09-07 04:56:27 +0000

    Correct copyright statement

--- src/ClientUtilities/util/SummaryVisitor.cs
+++ src/ClientUtilities/util/SummaryVisitor.cs
@@ -1,7 +1,7 @@
 // ****************************************************************
-// Copyright 2002-2003, Charlie Poole
-// This is free software licensed under the NUnit license. You may
-// obtain a copy of the license at http://nunit.org/?p=license&r=2.4
+// This is free software licensed under the NUnit license. You
+// may obtain a copy of the license as well as information regarding
+// copyright ownership at http://nunit.org/?p=license&r=2.4.
 // ****************************************************************
 
 namespace NUnit.Util
