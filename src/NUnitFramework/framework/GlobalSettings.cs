6e4da0b8e5c9af48e064e1fcf260d0f6ca202383
charliepoole <>
2007-10-10 18:54:24 +0000

    Add comments

--- src/NUnitFramework/framework/GlobalSettings.cs
+++ src/NUnitFramework/framework/GlobalSettings.cs
@@ -3,10 +3,14 @@ using System;
 namespace NUnit.Framework
 {
 	/// <summary>
-	/// Summary description for Settings.
+	/// GlobalSettings is a place for setting default values used
+	/// by the framework in performing asserts.
 	/// </summary>
 	public class GlobalSettings
 	{
+		/// <summary>
+		/// Default tolerance for floating point equality
+		/// </summary>
 		public static double DefaultFloatingPointTolerance = 0.0d;
 	}
 }
