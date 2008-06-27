e12d30da348972f39b3a9eb00d47fab4d6a94e26
charliepoole <>
2008-06-27 21:52:36 +0000

    Generate common assembly info for nant builds. Build number is based on the date.

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -1,23 +1,23 @@
 // ****************************************************************
 // This is free software licensed under the NUnit license. You
 // may obtain a copy of the license as well as information regarding
-// copyright ownership at http://nunit.org/?p=license&r=2.4.
+// copyright ownership at http://nunit.org.
 // ****************************************************************
 
 using System.Reflection;
 
 //
 // Common Information about all NUnit assemblies is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+// set of attributes. Change these source values to modify the information
+// associated with an assembly. 
 //
-//[assembly: AssemblyTitle("")]
-//[assembly: AssemblyDescription("")]
+// NOTE: This file is only used in the Visual Studio builds. For the NAnt builds,
+// an alternate file is generated and used.
+//
 [assembly: AssemblyCompany("NUnit.org")]
 [assembly: AssemblyProduct("NUnit")]
-[assembly: AssemblyCopyright("Copyright (C) 2002-2007 Charlie Poole.\r\nCopyright (C) 2002-2004 James W. Newkirk, Michael C. Two, Alexei A. Vorontsov.\r\nCopyright (C) 2000-2002 Philip Craig.\r\nAll Rights Reserved.")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]		
+[assembly: AssemblyCopyright("Copyright (C) 2002-2008 Charlie Poole.\r\nCopyright (C) 2002-2004 James W. Newkirk, Michael C. Two, Alexei A. Vorontsov.\r\nCopyright (C) 2000-2002 Philip Craig.\r\nAll Rights Reserved.")]
+[assembly: AssemblyTrademark("NUnit is a trademark of NUnit.org")]
 
 //
 // Version information for an assembly consists of the following four values:
@@ -30,6 +30,6 @@ using System.Reflection;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("2.5")]
-[assembly: AssemblyInformationalVersion("2.5")]
+[assembly: AssemblyVersion("2.5.0")]
+[assembly: AssemblyInformationalVersion("2.5.0")]
 [assembly: AssemblyConfiguration("Alpha-3")]
