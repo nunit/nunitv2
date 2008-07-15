2e2dd86aff8038f07a6a90fede6139c14058163d
charliepoole <>
2008-07-15 18:11:26 +0000

    Add AssemblyHelper class to support common operations on assemblies

---
+++ src/NUnitCore/core/AssemblyHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reflection;
+
+namespace NUnit.Core
+{
+    public class AssemblyHelper
+    {
+        #region GetAssemblyPath
+        public static string GetAssemblyPath(Assembly assembly)
+        {
+            string path = assembly.CodeBase;
+            Uri uri = new Uri(path);
+
+            // If it wasn't loaded locally, use the Location
+            if (!uri.IsFile)
+                return assembly.Location;
+
+            if (uri.IsUnc)
+                return path.Substring(Uri.UriSchemeFile.Length + 1);
+
+
+            int start = Uri.UriSchemeFile.Length + Uri.SchemeDelimiter.Length;
+
+            if (path[start] == '/' && path[start + 2] == ':')
+                ++start;
+
+            return path.Substring(start);
+        }
+        #endregion
+
+        #region GetDirectoryName
+        public static string GetDirectoryName( Assembly assembly )
+        {
+            return System.IO.Path.GetDirectoryName(GetAssemblyPath(assembly));
+        }
+        #endregion
+    }
+}
