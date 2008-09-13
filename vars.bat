aab333a2d9d6f3a41f2735594a4c5404b97839f7
charliepoole <>
2008-09-13 18:46:18 +0000

    Remove msbuild items used only for the TD.Net build of NUnit 2.2.

--- vars.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-@echo This build environment requires .NET Framework 2.0 RTM (v2.0.50727)
-@echo To build use 'msbuild NUnit.proj'
-
-set path=%path%;%SystemRoot%\Microsoft.NET\Framework\v2.0.50727
