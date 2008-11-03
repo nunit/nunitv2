613197cf085418c33384315a2174d38092aefbbd
charliepoole <>
2008-11-03 18:33:21 +0000

    Move MaxTime from extensions into NUnit itself.

--- src/NUnitFramework/framework/MaxTimeAttribute.cs
+++ src/NUnitFramework/framework/MaxTimeAttribute.cs
@@ -6,24 +6,15 @@
 
 using System;
 
-namespace NUnit.Framework.Extensions
+namespace NUnit.Framework
 {
 	/// <summary>
 	/// Summary description for MaxTimeAttribute.
 	/// </summary>
 	[AttributeUsage( AttributeTargets.Method, AllowMultiple=false, Inherited=false )]
-	public sealed class MaxTimeAttribute : Attribute
+	public sealed class MaxTimeAttribute : PropertyAttribute
 	{
-		private readonly int maxTime;
-
 		public MaxTimeAttribute( int maxTime )
-		{
-			this.maxTime = maxTime;
-		}
-
-		public int MaxTime
-		{
-			get { return maxTime; }
-		}
+            : base( maxTime ) { }
 	}
 }
