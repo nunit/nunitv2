6d119d997458a75114f091eafd3e42081741c738
charliepoole <>
2008-12-30 22:23:36 +0000

    Display <type>.MinValue and <type>.MaxValue as argument for a parameterized test when the data provided is equal to the min and max values. (Bug #2477016)

--- src/NUnitCore/core/MethodHelper.cs
+++ src/NUnitCore/core/MethodHelper.cs
@@ -37,72 +37,105 @@ namespace NUnit.Core
                 for (int i = 0; i < arglist.Length; i++)
                 {
                     if (i > 0) sb.Append(",");
-
-                    object arg = arglist[i];
-                    string display = arg == null ? "null" : arg.ToString();
-
-                    if (arg is double)
-                    {
-                        double d = (double)arg;
-
-                        if (double.IsNaN(d))
-                            display = "double.NaN";
-                        else if (double.IsPositiveInfinity(d))
-                            display = "double.PositiveInfinity";
-                        else if (double.IsNegativeInfinity(d))
-                            display = "double.NegativeInfinity";
-                        else if (d == double.MaxValue)
-                            display = "double.MaxValue";
-                        else if (d == double.MinValue)
-                            display = "double.MinValue";
-                        else
-                        {
-                            if (display.IndexOf('.') == -1)
-                                display += ".0";
-                            display += "d";
-                        }
-                    }
-                    else if (arg is float)
-                    {
-                        float f = (float)arg;
-
-                        if (float.IsNaN(f))
-                            display = "float.NaN";
-                        else if (float.IsPositiveInfinity(f))
-                            display = "float.PositiveInfinity";
-                        else if (float.IsNegativeInfinity(f))
-                            display = "float.NegativeInfinity";
-                        else if (f == float.MaxValue)
-                            display = "float.MaxValue";
-                        else if (f == float.MinValue)
-                            display = "float.MinValue";
-                        else
-                        {
-                            if (display.IndexOf('.') == -1)
-                                display += ".0";
-                            display += "f";
-                        }
-                    }
-                    else if (arg is decimal) display += "m";
-                    else if (arg is long) display += "L";
-                    else if (arg is ulong) display += "UL";
-                    else if (arg is string) display = "\"" + display + "\"";
-                    else if (arg is int)
-                    {
-                        int ival = (int)arg;
-                        if (ival == int.MaxValue)
-                            display = "int.MaxValue";
-                        else if (ival == int.MinValue)
-                            display = "int.MinValue";
-                    }
-
-                    sb.Append(display);
+                    sb.Append(GetDisplayString(arglist[i]));
                 }
 
                 sb.Append(")");
             }
 
             return sb.ToString();
+        }
+
+        private static string GetDisplayString(object arg)
+        {
+            string display = arg == null ? "null" : arg.ToString();
+
+            if (arg is double)
+            {
+                double d = (double)arg;
+
+                if (double.IsNaN(d))
+                    display = "double.NaN";
+                else if (double.IsPositiveInfinity(d))
+                    display = "double.PositiveInfinity";
+                else if (double.IsNegativeInfinity(d))
+                    display = "double.NegativeInfinity";
+                else if (d == double.MaxValue)
+                    display = "double.MaxValue";
+                else if (d == double.MinValue)
+                    display = "double.MinValue";
+                else
+                {
+                    if (display.IndexOf('.') == -1)
+                        display += ".0";
+                    display += "d";
+                }
+            }
+            else if (arg is float)
+            {
+                float f = (float)arg;
+
+                if (float.IsNaN(f))
+                    display = "float.NaN";
+                else if (float.IsPositiveInfinity(f))
+                    display = "float.PositiveInfinity";
+                else if (float.IsNegativeInfinity(f))
+                    display = "float.NegativeInfinity";
+                else if (f == float.MaxValue)
+                    display = "float.MaxValue";
+                else if (f == float.MinValue)
+                    display = "float.MinValue";
+                else
+                {
+                    if (display.IndexOf('.') == -1)
+                        display += ".0";
+                    display += "f";
+                }
+            }
+            else if (arg is decimal)
+            {
+                decimal d = (decimal)arg;
+                if (d == decimal.MinValue)
+                    display = "decimal.MinValue";
+                else if (d == decimal.MaxValue)
+                    display = "decimal.MaxValue";
+                else
+                    display += "m";
+            }
+            else if (arg is long)
+            {
+                long l = (long)arg;
+                if (l == long.MinValue)
+                    display = "long.MinValue";
+                else if (l == long.MinValue)
+                    display = "long.MaxValue";
+                else
+                    display += "L";
+            }
+            else if (arg is ulong)
+            {
+                ulong ul = (ulong)arg;
+                if (ul == ulong.MinValue)
+                    display = "ulong.MinValue";
+                else if (ul == ulong.MinValue)
+                    display = "ulong.MaxValue";
+                else
+                    display += "UL";
+            }
+            else if (arg is string)
+            {
+                display = "\"" + display + "\"";
+            }
+            else if (arg is int)
+            {
+                int ival = (int)arg;
+                if (ival == int.MaxValue)
+                    display = "int.MaxValue";
+                else if (ival == int.MinValue)
+                    display = "int.MinValue";
+            }
+
+            return display;
         }
     }
 }
