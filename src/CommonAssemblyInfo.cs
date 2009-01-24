120f52378d18bb85e7e2f54e16f222c9e2c96f5c
charliepoole <>
2009-01-24 21:09:13 +0000

    Advance to beta-3 designation

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -32,4 +32,4 @@ using System.Reflection;
 
 [assembly: AssemblyVersion("2.5.0")]
 [assembly: AssemblyInformationalVersion("2.5.0")]
-[assembly: AssemblyConfiguration("Beta-1")]
+[assembly: AssemblyConfiguration("Beta-3")]
