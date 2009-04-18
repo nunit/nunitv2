7faa6df5bb27277415fb3c8d96630e2e00e5fd49
charliepoole <>
2009-04-18 20:57:20 +0000

    Update license header text

--- src/NUnitCore/interfaces/OSPlatform.cs
+++ src/NUnitCore/interfaces/OSPlatform.cs
@@ -1,7 +1,7 @@
 ﻿// ****************************************************************
 // Copyright 2008, Charlie Poole
 // This is free software licensed under the NUnit license. You may
-// obtain a copy of the license at http://nunit.org/?p=license&r=2.4
+// obtain a copy of the license at http://nunit.org
 // ****************************************************************
 using System;
 using System.Runtime.InteropServices;
