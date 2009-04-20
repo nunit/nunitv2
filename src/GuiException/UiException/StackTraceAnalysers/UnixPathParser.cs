170587f4a9c7e027ff17023743820eb8f8e3f779
charliepoole <>
2009-04-20 06:00:51 +0000

    Update license header text

--- src/GuiException/UiException/StackTraceAnalysers/UnixPathParser.cs
+++ src/GuiException/UiException/StackTraceAnalysers/UnixPathParser.cs
@@ -1,10 +1,7 @@
-﻿// ----------------------------------------------------------------
-// ErrorBrowser
-// Written by Irénée HOTTIER, 2009, FRANCE
-// 
-// This is free software licensed under the NUnit license, You may
-// obtain a copy of the license at http://nunit.org/?p=license&r=2.4
-// ----------------------------------------------------------------
+﻿// ****************************************************************
+// This is free software licensed under the NUnit license. You may
+// obtain a copy of the license at http://nunit.org
+// ****************************************************************
 
 using System;
 using System.Collections.Generic;
--- src/GuiException/tests/TestPaintLineLocation.cs
+++ src/GuiException/tests/TestPaintLineLocation.cs
@@ -1,10 +1,7 @@
-﻿// ----------------------------------------------------------------
-// ErrorBrowser
-// Copyright 2008-2009, Irénée HOTTIER,
-// 
-// This is free software licensed under the NUnit license, You may
-// obtain a copy of the license at http://nunit.org/?p=license&r=2.4
-// ----------------------------------------------------------------
+﻿// ****************************************************************
+// This is free software licensed under the NUnit license. You may
+// obtain a copy of the license at http://nunit.org
+// ****************************************************************
 
 using System;
 using System.Collections.Generic;
