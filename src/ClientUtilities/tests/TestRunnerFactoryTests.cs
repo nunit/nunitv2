f0db2ff368b234e2a9cb8c008f4f4f8518b9ad4d
Charlie Poole <charlie@nunit.com>
2010-04-04 11:01:28 -0700

    Add tests for TestRunnerFactory and RuntimeFrameoworkSelector

---
+++ src/ClientUtilities/tests/TestRunnerFactoryTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NUnit.Core;
+using NUnit.Framework;
+
+namespace NUnit.Util.Tests
+{
+    [TestFixture]
+    public class TestRunnerFactoryTests
+    {
+        private RuntimeFramework currentFramework = RuntimeFramework.CurrentFramework;
+
+        [Test]
+        public void SameFrameworkUsesTestDomain()
+        {
+            TestPackage package = new TestPackage("nunit.util.tests.dll");
+            Assert.That( new TestRunnerFactory().MakeTestRunner(package),
+                Is.TypeOf(typeof(TestDomain)));
+        }
+    }
+}
