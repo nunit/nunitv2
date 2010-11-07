ff8ba1d233bf0a0cca85e6b86a10b43ec245af5f
Charlie Poole <charlie@nunit.org>
2010-11-07 07:49:05 -0800

    Move bin directory as well as NUnitTests.nunit and NUnitTests.config from src to root of project

--- src/NUnitCore/core/AssemblyInfo.cs
+++ src/NUnitCore/core/AssemblyInfo.cs
@@ -9,5 +9,5 @@ using System.Reflection;
 [assembly: CLSCompliant(true)]
 
 [assembly: AssemblyDelaySign(false)]
-[assembly: AssemblyKeyFile("../../../../nunit.snk")]
+[assembly: AssemblyKeyFile("../../nunit.snk")]
 [assembly: AssemblyKeyName("")]
