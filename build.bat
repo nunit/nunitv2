86c801cd914a3c0bb38d6fe93fa7fc9ab302ea4b
Charlie Poole <charlie@nunit.org>
2011-03-04 21:18:46 -0800

    Refactor wxs files and scripts

--- build.bat
+++ build.bat
@@ -1,7 +1,14 @@
+@echo off
+
+rem Wrapper script for nant build targets
+
+setlocal
+
 set NANT=tools\NAnt-0.90\bin\nant.exe
 set OPTIONS=-f:scripts\nunit.build.targets
 set CONFIG=
 set TARGET=
+set CLEAN=
 set COMMANDS=
 goto start
 
@@ -9,6 +16,11 @@ goto start
 shift /1
 
 :start
+
+IF /I "%1" EQU "?"	goto usage
+IF /I "%1" EQU "/h"	goto usage
+IF /I "%1" EQU "/help"	goto usage
+
 IF /I "%1" EQU "debug" set CONFIG=debug&goto shift
 IF /I "%1" EQU "release" set CONFIG=release&goto shift
 
@@ -19,13 +31,17 @@ IF /I "%1" EQU "net-2.0" set TARGET=net-2.0&goto shift
 IF /I "%1" EQU "net-3.0" set TARGET=net-3.0&goto shift
 IF /I "%1" EQU "net-3.5" set TARGET=net-3.5&goto shift
 IF /I "%1" EQU "net-4.0" set TARGET=net-4.0&goto shift
+
 IF /I "%1" EQU "mono" set TARGET=mono&goto shift
 IF /I "%1" EQU "mono-1.0" set TARGET=mono-1.0&goto shift
 IF /I "%1" EQU "mono-2.0" set TARGET=mono-2.0&goto shift
 IF /I "%1" EQU "mono-3.5" set TARGET=mono-3.5&goto shift
 IF /I "%1" EQU "mono-4.0" set TARGET=mono-4.0&goto shift
 
+if "%1" EQU "clean" set CLEAN=clean&goto shift
 IF "%1" EQU "samples" set COMMANDS=%COMMANDS% build-samples&goto shift
+IF "%1" EQU "tools" set COMMANDS=%COMMANDS% build-tools&goto shift
+
 IF "%1" NEQ "" set COMMANDS=%COMMANDS% %1&goto shift
 
 if "%CONFIG%" NEQ "" set OPTIONS=%OPTIONS% -D:build.config=%CONFIG%
@@ -33,4 +49,40 @@ if "%TARGET%" NEQ "" set OPTIONS=%OPTIONS% -D:runtime.config=%TARGET%
 
 if "%COMMANDS%" EQU "" set COMMANDS=build
 
-%NANT% %OPTIONS% %COMMANDS%
+%NANT% %OPTIONS% %CLEAN% %COMMANDS%
+
+goto done
+
+: usage
+
+echo Builds and tests NUnit for various targets
+echo.
+echo usage: BUILD [option [...] ]
+echo.
+echo Options may be any of the following, in any order...
+echo.
+echo   debug          Builds debug configuration (default)
+echo   release        Builds release configuration
+echo.
+echo   net-4.0        Targets .NET 4.0 (future)
+echo   net-2.0        Targets .NET 2.0 (default)
+echo   net-1.1        Targets .NET 1.1
+echo   net-1.0        Targets .NET 1.0
+echo.
+echo   clean          Cleans the output directory before building
+echo.
+echo   samples        Builds the NUnit samples
+echo   tools          Builds the NUnit tools
+echo.
+echo   test           Runs tests for a build using the console runner
+echo   gui-test       Runs tests for a build using the NUnit gui
+echo.
+echo   ?, /h, /help   Displays this help message
+echo.
+echo In addition, any valid target in the NAnt script may
+echo be supplied as an argument. This requires some degree
+echo of familiarity with the script, in order to avoid
+echo use of incompatible options.
+echo.   
+
+: done
--- package.bat
+++ package.bat
@@ -1,3 +1,9 @@
+@echo off
+
+rem Wrapper script for nant packaging targets
+
+setlocal
+
 set NANT=tools\NAnt-0.90\bin\nant.exe
 set OPTIONS=-f:scripts/nunit.package.targets
 set CONFIG=
@@ -9,7 +15,12 @@ goto start
 shift /1
 
 :start
-IF /I "%1" EQU "debug" set CONFIG=debug&goto shift
+
+IF /I "%1" EQU "?"	goto usage
+IF /I "%1" EQU "/h"	goto usage
+IF /I "%1" EQU "/help"	goto usage
+
+IF /I "%1" EQU "debug"	set CONFIG=debug&goto shift
 IF /I "%1" EQU "release" set CONFIG=release&goto shift
 
 IF /I "%1" EQU "net"	 set TARGET=net&goto shift
@@ -33,9 +44,43 @@ IF /I "%1" EQU "src"	set COMMANS=%COMMANDS% package-src&goto shift
 
 IF "%1" NEQ "" set COMMANDS=%COMMANDS% %1&goto shift
 
-if "%CONFIG%" NEQ "" set OPTIONS=%OPTIONS% -D:build.config=%CONFIG%
-if "%TARGET%" NEQ "" set OPTIONS=%OPTIONS% -D:runtime.config=%TARGET%
+IF "%CONFIG%" EQU "" set CONFIG=release
+set OPTIONS=%OPTIONS% -D:build.config=%CONFIG%
+
+IF "%TARGET%" NEQ "" set OPTIONS=%OPTIONS% -D:runtime.config=%TARGET%
 
 if "%COMMANDS%" EQU "" set COMMANDS=package
 
 %NANT% %OPTIONS% %COMMANDS%
+
+goto done
+
+:usage
+
+echo Builds one or more NUnit packages for distribution
+echo.
+echo usage: PACKAGE [option [...] ]
+echo.
+echo Options may be any of the following, in any order...
+echo.
+echo   release        Builds packages for release (default)
+echo   debug          Builds debug packages
+echo.
+echo   net-4.0        Builds packages for .NET 4.0 (future)
+echo   net-2.0        Builds packages for .NET 2.0 (default)
+echo   net-1.1        Builds packages for .NET 1.1
+echo   net-1.0        Builds packages for .NET 1.0
+echo.
+echo   src, source    Builds the source package
+echo   docs           Builds the documentation package
+echo   all            Builds all packages for all platforms
+echo.
+echo   ?, /h, /help   Displays this help message
+echo.
+echo In addition, any valid target in the NAnt script may
+echo be supplied as an argument. This requires some degree
+echo of familiarity with the script, in order to avoid
+echo use of incompatible options.
+echo.   
+
+:done
