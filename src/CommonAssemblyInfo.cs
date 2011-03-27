112204bb295621899e820ab19f106dd8cc758845
Charlie Poole <charlie@nunit.org>
2011-03-27 10:38:53 -0700

    Update copyright year

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 //
 [assembly: AssemblyCompany("NUnit.org")]
 [assembly: AssemblyProduct("NUnit")]
-[assembly: AssemblyCopyright("Copyright (C) 2002-2010 Charlie Poole.\r\nCopyright (C) 2002-2004 James W. Newkirk, Michael C. Two, Alexei A. Vorontsov.\r\nCopyright (C) 2000-2002 Philip Craig.\r\nAll Rights Reserved.")]
+[assembly: AssemblyCopyright("Copyright (C) 2002-2011 Charlie Poole.\r\nCopyright (C) 2002-2004 James W. Newkirk, Michael C. Two, Alexei A. Vorontsov.\r\nCopyright (C) 2000-2002 Philip Craig.\r\nAll Rights Reserved.")]
 [assembly: AssemblyTrademark("NUnit is a trademark of NUnit.org")]
 
 //
