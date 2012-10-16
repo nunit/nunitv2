e194a4af0f6a8573e54a62fa1e36896b6371cf1c
Simone Busoli <simone.busoli@vienna>
2012-10-16 02:48:41 +0200

    Support for async void test methods containing code which invokes async void methods.

--- src/NUnitCore/core/AsyncSynchronizationContext.cs
+++ src/NUnitCore/core/AsyncSynchronizationContext.cs
@@ -1,18 +1,13 @@
 using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.Threading;
 
 namespace NUnit.Core
 {
 	public class AsyncSynchronizationContext : SynchronizationContext
     {
-        private readonly AutoResetEvent _operationCompleted = new AutoResetEvent(false);
-        private readonly IList<Exception> _exceptions = new List<Exception>();
-
-		public IList<Exception> Exceptions
-        {
-            get { return _exceptions; }
-        }
+        private int _operationCount;
+		private readonly AsyncOperationQueue _operations = new AsyncOperationQueue();
 
 		public override void Send(SendOrPostCallback d, object state)
 		{
@@ -20,26 +15,83 @@ namespace NUnit.Core
 		}
 
 		public override void Post(SendOrPostCallback d, object state)
-        {
-			try
-			{
-				d(state);
-			}
-			catch (Exception e)
-			{
-				_exceptions.Add(e);
-			}
+		{
+			_operations.Enqueue(new AsyncOperation(d, state));
+		}
+
+		public override void OperationStarted()
+		{
+			Interlocked.Increment(ref _operationCount);
+			base.OperationStarted();
+		}
+
+		public override void OperationCompleted()
+		{
+			if (Interlocked.Decrement(ref _operationCount) == 0)
+				_operations.MarkAsComplete();
+
+			base.OperationCompleted();
         }
 
-        public override void OperationCompleted()
-        {
-            base.OperationCompleted();
-            _operationCompleted.Set();
-        }
+		public void WaitForPendingOperationsToComplete()
+		{
+			_operations.InvokeAll();
+		}
 
-        public void WaitForOperationCompleted()
-        {
-            _operationCompleted.WaitOne();
-        }
+		private class AsyncOperationQueue
+		{
+			private bool _run = true;
+			private readonly Queue _operations = Queue.Synchronized(new Queue());
+			private readonly AutoResetEvent _operationsAvailable = new AutoResetEvent(false);
+
+			public void Enqueue(AsyncOperation asyncOperation)
+			{
+				_operations.Enqueue(asyncOperation);
+				_operationsAvailable.Set();
+			}
+
+			public void MarkAsComplete()
+			{
+				_run = false;
+				_operationsAvailable.Set();
+			}
+
+			public void InvokeAll()
+			{
+				while (_run)
+				{
+					InvokePendingOperations();
+					_operationsAvailable.WaitOne();
+				}
+
+				InvokePendingOperations();
+			}
+
+			private void InvokePendingOperations()
+			{
+				while (_operations.Count > 0)
+				{
+					AsyncOperation operation = (AsyncOperation)_operations.Dequeue();
+					operation.Invoke();
+				}
+			}
+		}
+
+		private class AsyncOperation
+		{
+			private readonly SendOrPostCallback _action;
+			private readonly object _state;
+
+			public AsyncOperation(SendOrPostCallback action, object state)
+			{
+				_action = action;
+				_state = state;
+			}
+
+			public void Invoke()
+			{
+				_action(_state);
+			}
+		}
     }
 }
